ef97fbaabf0e12ab5b684b7e43fa4aaf4a69a1af
Myperfectgame <various.artists@yahoo.com>
2014-05-02 22:39:08 +0100

    Revert "deleting duplicates"
    This reverts commit d5d82e6ae2eae0cf54f14bd533ab9ce143d30a52.

---
+++ src/main/java/com/myperfectgame/IBlock.java
@@ -0,0 +1,36 @@
+package com.myperfectgame;
+
+import org.newdawn.slick.Color;
+
+public class IBlock extends AbstractBlock implements Block {
+
+	public IBlock() {
+		blocks = new boolean[][] { 
+		{ false, false, true, false, },
+		{ false, false, true, false, },
+		{ false, false, true, false, },
+		{ false, false, true, false, } };
+	}
+
+	@Override
+	public Color getColor() {
+		return Config.IBLOCKCOLOUR;
+	}
+	
+	@Override
+	public boolean[][] rotateClockWise() {
+		if(blocks[3][1] == true) {
+			return super.rotateCounterClockWise();
+		}
+		else return super.rotateClockWise();
+	}
+
+	@Override
+	public boolean[][] rotateCounterClockWise() {
+		if(blocks[1][3] == true) {
+			return super.rotateClockWise();
+		}
+		else return super.rotateCounterClockWise();
+	}
+
+}
