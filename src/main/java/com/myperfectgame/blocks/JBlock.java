f083984cbb1b431737680718d5823e5e9811b5a2
Oddvar Lovaas <various.artists@yahoo.com>
2014-08-02 00:02:11 +0100

    better jblock image

--- src/main/java/com/myperfectgame/blocks/JBlock.java
+++ src/main/java/com/myperfectgame/blocks/JBlock.java
@@ -17,10 +17,10 @@ public class JBlock extends AbstractBlock<Image> implements Block<Image> {
 
         try {
             SpriteSheet spriteSheet = new SpriteSheet( new Image("resources/jblock.png"), 30, 30);
-            blocks[0][2] = spriteSheet.getSprite(1, 0);
+            blocks[0][2] = spriteSheet.getSprite(0, 1);
             blocks[1][2] = spriteSheet.getSprite(1, 1);
-            blocks[2][2] = spriteSheet.getSprite(1, 2);
-            blocks[2][1] = spriteSheet.getSprite(0, 2);
+            blocks[2][2] = spriteSheet.getSprite(2, 1);
+            blocks[2][1] = spriteSheet.getSprite(2, 0);
         }
         catch(SlickException e) {}
 
